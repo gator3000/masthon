a3c9dafb6522f0c551f4076e8a5ded54c585cf5d
Gator3000 <aelian.brd@proton.me>
2025-04-24 10:41:48 +0200

    some changes

--- masthon/Client.py
+++ masthon/Client.py
@@ -23,10 +23,14 @@ class Client:
     def __repr__(self) -> str:
         return f"Client(token='{self.token[:7] + '*'*(20-7) + self.token[20:]}')"
 
+    def stop(self) -> None:
+        self.RUNNING = False
+
     def _step(self, i: int = None) -> Any:
         pass
 
     def run(self) -> None:
+        if self.RUNNING:raise RuntimeError("You can't run two instances at the same time.")
         i = 0
         self.RUNNING = True
         while self.RUNNING:
@@ -35,3 +39,20 @@ class Client:
             except BaseException as e:
                 print(e)
             i += 1
+    
+    def _run(self):
+        if self.RUNNING:raise RuntimeError("You can't run two instances at the same time.")
+        i = 0
+        self.RUNNING = True
+        while self.RUNNING:
+            try:
+                yield i, self._step(i)
+            except BaseException as e:
+                print(e)
+                yield None
+            i += 1
+    
+    def __iter__(self):
+        for i, return_ in self._run():
+            yield i, return_
+    
