c93a7c0fe8ede0420c8b44eba79045926f00927d
Gator 3000 <aelian.brd@proton.me>
2025-04-29 17:58:37 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,6 +14,6 @@ setup(
     author_email='org.literie.gator@h3110.aleeas.com',
     license='GNU/GPL v3',
     packages=['masthon'],
-    zip_safe=False
+    zip_safe=False,
     python_requires = ">=3.10"
 )
