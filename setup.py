d0e21af91b6467bfc30f232c3fde38283f6634fc
Gator3000 <aelian.brd@proton.me>
2025-04-30 23:32:56 +0200

    adjust version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
     
 setup(
     name='Masthon',
-    version='v0.1a',
+    version='v0.1b',
     description='A simple API for linking masthodon to your python codes.',
     long_description = long_description,
     long_description_content_type = "text/markdown",
