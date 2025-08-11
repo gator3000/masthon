954868c9ff0804857d1aafbb23fd026be2bef78c
sephiroth <avivies971@gmail.com>
2025-08-11 20:05:52 +0200

    variable global apiversion

--- masthon/Client.py
+++ masthon/Client.py
@@ -51,7 +51,7 @@ class Client:
     def __init__(
         self,
         token: str,
-        server: str = "https://mastodon.social",
+        server: str = f"https://mastodon.social{apiversion}",
         *,
         async_level: int = 0,
         used_events: Optional[Event | Tuple[Event]] = None,
