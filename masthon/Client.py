689596ab6f03e510eea0393c71633944eab7938a
sephiroth <avivies971@gmail.com>
2025-08-13 15:16:20 +0200

    version api

--- masthon/Client.py
+++ masthon/Client.py
@@ -21,7 +21,8 @@ import traceback
 TOKEN_FORMAT: re.Pattern = re.compile(r"^[A-Za-z0-9\-_]{43}$")
 # SERVER_FORMAT: re.Pattern = re.compile(r"^(http(s)?:\/\/)?([a-zA-Z0-9-]{1,61}\.){1,}[a-zA-Z]{2,}$")
 SERVER_FORMAT: re.Pattern = re.compile( r"^https?://([a-zA-Z0-9-]{1,61}\.)+[a-zA-Z]{2,}(/api/v[0-9]+)?/?$") #pour ajouter api/v1
-apiversion="/api/v1"
+apiversion1="/api/v1"
+apiversion2="/api/v2"
 class Client:
     """
     A class representing your application.
@@ -30,7 +31,7 @@ class Client:
     def __init__(
         self,
         token: str,
-        server: str = f"https://mastodon.social{apiversion}",
+        server: str = f"https://mastodon.social",
         *,
         async_level: int = 0,
         used_events: Optional[Event | Tuple[Event]] = None,
@@ -275,7 +276,7 @@ class Client:
         Returns:
             requests.Response: The response from the API.
         """
-        url = self.server + apiversion + path
+        url = self.server + path
         print(url)
         auth = {"Authorization": f"Bearer {self.token}"} if not annonymous else dict()
         data = {**kwargs, **additional_data}
@@ -354,7 +355,7 @@ class Client:
 
         
         response = self._raw_request(
-            path="/statuses",
+            path=f"{apiversion1}/statuses",
             method=RequestMethod.POST,
             status=text,
             visibility=visibility.value,
