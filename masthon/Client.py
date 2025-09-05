f69e78d8eb7b7ed66503cf88324f6c8d813290b1
sephiroth <avivies971@gmail.com>
2025-09-05 22:51:36 +0200

    pipeline test

--- masthon/Client.py
+++ masthon/Client.py
@@ -528,7 +528,7 @@ class Client:
         
     @LOG()
     def Token_access(self,clientid :str,clientsecret:str,code: str)-> str:
-        "permet de récuperer le token d'access"
+        "permet de récuperer le token d'access "
         try:
             url="/oauth/token"
             data = {
